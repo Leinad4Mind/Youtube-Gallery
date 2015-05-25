63424b779e09f3bcc63e261216faed5805d7b6d9
Daniel Mota <leinad4mind@gmail.com>
2015-05-25 19:29:12 +0100

    Update permissions_video.php

--- phpbb3_youtube_gallery/root/language/pt/mods/permissions_video.php
+++ phpbb3_youtube_gallery/root/language/pt/mods/permissions_video.php
@@ -30,9 +30,9 @@ $lang['permission_cat']['video'] = 'Galeria de Vídeo';
 
 // Adding the permissions
 $lang = array_merge($lang, array(
-	'acl_u_video_view_full'	=> array('lang'	=> 'Pode ver a Galeria de Vídeo',	'cat' => 'video'),
-	'acl_u_video_view'		=> array('lang'	=> 'Pode ver vídeos',			'cat' => 'video'),
-	'acl_u_video_delete'	=> array('lang' => 'Pode apagar os seus próprios vídeos',		'cat' => 'video'),
-	'acl_u_video_post'		=> array('lang'	=> 'Pode colocar vídeos',			'cat' => 'video'),
+	'acl_u_video_view_full'	=> array('lang'	=> 'Pode ver a Galeria de Vídeo',		'cat' => 'video'),
+	'acl_u_video_view'	=> array('lang'	=> 'Pode ver vídeos',				'cat' => 'video'),
+	'acl_u_video_delete'	=> array('lang' => 'Pode apagar os seus próprios vídeos',	'cat' => 'video'),
+	'acl_u_video_post'	=> array('lang'	=> 'Pode colocar vídeos',			'cat' => 'video'),
 ));
 ?>
